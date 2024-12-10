8efb6669b550104441e9391c28affc2e229d339c
mhmd329 <mhmdkhaled092@gmail.com>
2024-12-11 00:43:21 +0200

    add startup files

--- src/App.js
+++ src/App.js
@@ -1,13 +1,24 @@
+import { Container } from 'react-bootstrap';
 import './App.css';
+import { Routes ,Route} from 'react-router-dom';
+import { Home } from './components/Home';
+import { About } from './components/About';
+import { Store } from './components/Store';
+import { Navbar } from './components/Navbar';
 
 function App() {
   return (
-    <div >
-     <button>
-      click
-     </button>
-    </div>
-  );
+    <>
+    <Navbar/>
+  <Container className='mb-4'>
+<Routes>
+<Route path='/' element={<Home/>}/>
+<Route path='/Store' element={<Store/>}/>
+<Route path='/About' element={<About/>}/>
+</Routes>
+  </Container>
+  </>
+  )
 }
 
 export default App;
